6350ae9b673d866937ef11dba70756f76011b2cc
Holly Williams <hollyyeh12@gmail.com>
2023-06-15 16:39:50 +0000

    setting up django settings

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
-def say_hello(request):
-    return HttpResponse("Hello!")
+def get_todo_list(request):
+    return render(request, '/workspace/hellodjango/todo/templates/todo2/todo_list.html')
