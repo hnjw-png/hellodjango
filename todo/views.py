a2d9a523245c9df10e1b128cb97d4ea8439db7ab
Holly Williams <hollyyeh12@gmail.com>
2023-06-19 12:38:20 +0000

    add item url

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
 
 def get_todo_list(request):
-    return render(request, '/workspace/hellodjango/todo/templates/todo2/todo_list.html')
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, '/workspace/hellodjango/todo/templates/todo2/todo_list.html', context)
+    
+def add_item(request):
+    return render(request, 'todo/templates/todo2/add_item.html')
