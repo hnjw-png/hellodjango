12a988e4b16989ac7796b68eb302d9a27210b465
Holly Williams <hollyyeh12@gmail.com>
2023-06-19 13:17:16 +0000

    add item page

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Item
 
 # Create your views here.
@@ -11,4 +11,11 @@ def get_todo_list(request):
     return render(request, '/workspace/hellodjango/todo/templates/todo2/todo_list.html', context)
     
 def add_item(request):
-    return render(request, 'todo/templates/todo2/add_item.html')
+    if request.method == 'POST':
+        name = request.POST.get('item_name')
+        done = 'done' in request.POST
+        Item.objects.create(name=name, done=done)
+
+        return redirect('get_todo_list')
+
+    return render(request, 'todo/add_item.html')
